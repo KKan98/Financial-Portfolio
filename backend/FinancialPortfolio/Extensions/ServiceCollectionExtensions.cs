fb3481fcbde98e0d4e1ad3a885b85c914afd313a
Kacper Kania <kacper.kania@atos.net>
2026-09-05 19:57:01 +0200

    Create extensions to swagger options

---
+++ backend/FinancialPortfolio/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace FinancialPortfolio.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "JWT Token",
+                    Description = "Bearer Token: ",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        []
+                    }
+                });
+            });
+
+            return services;
+        }
+
+    }
+}
--- backend/FinancialPortfolio/Program.cs
+++ backend/FinancialPortfolio/Program.cs
@@ -2,7 +2,7 @@ using FinancialPortfolio.Services.Users;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.OpenApi.Models;
+using FinancialPortfolio.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,34 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "JWT Token",
-        Description = "Bearer Token: ",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = JwtBearerDefaults.AuthenticationScheme
-                }
-            }, 
-            []
-        }
-    });
-});
+builder.Services.AddSwaggerGenWithAuth();
 
 builder.Services.AddScoped<IUserService, UserService>();
 
