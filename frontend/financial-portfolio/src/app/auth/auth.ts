5e2edfe59320d8d079b997825ccafa46456f2d3a
Kacper Kania <kacper.kania@atos.net>
2026-09-06 13:43:29 +0200

    add: authService, make auth.html ReactiveFormsModule

--- frontend/financial-portfolio/src/app/auth/auth.ts
+++ frontend/financial-portfolio/src/app/auth/auth.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-auth',
   styleUrl: './auth.css',
   templateUrl: './auth.html',
 })
-export class Auth {}
+export class Auth {
+  constructor(private authService: AuthService) {}
+
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+
+    if(typeof enteredEmail === 'string' && typeof enteredPassword === 'string') {
+      this.authService.login(enteredEmail, enteredPassword);
+    } else {
+      console.log("Enter E-mail and Password");
+    }
+  }
+}
